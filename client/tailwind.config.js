bd2293b8bde4bbb05ab1973fa4946be167651914
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-02-05 14:01:43 +0630

    config and assets

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-bg" : "#08281D"
+      }
+    },
   },
   plugins: [],
 }
