aa14ec4cbd73492416337fae2ab73fc3778221de
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-02-18 14:04:36 +0630

    changed: codes

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,18 +1,20 @@
 import './globals.css'
+import Layout from '@src/layout/client'
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Layout>
+          {children}
+        </Layout>
+      </body>
     </html>
   )
 }
